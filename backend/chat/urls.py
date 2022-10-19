5ef579dac30a9c41cde7d08780cca266b2487a71
drjjjm18 <71101450+drjjjm18@users.noreply.github.com>
2022-10-19 12:01:34 +0100

    remove reference to views

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from app.views import room
 
 urlpatterns = [
     path('admin/', admin.site.urls),
